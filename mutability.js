89dfd9f14d4abe2fa9a2d2945600d43c550164f4
ajaberi <adnan.ajaberi@gmail.com>
2023-11-26 20:23:00 +0300

    done

---
+++ mutability.js
@@ -0,0 +1,13 @@
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+//   }
+
+let clone1 = { ...person };
+let clone2 = Object.assign({}, person);
+
+let samePerson = person;
+
+person.age += 1;
+person.country = "FR";
