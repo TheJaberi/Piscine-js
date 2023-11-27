cd3a9d93d9034c3c91424c4f37e6825513ad71d5
ajaberi <adnan.ajaberi@gmail.com>
2023-11-27 14:37:22 +0300

    done

---
+++ sign.js
@@ -0,0 +1,12 @@
+function sign(num) {
+  if (num > 0) {
+    return 1;
+  } else if (num < 0) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+function sameSign(a, b) {
+  return sign(a) === sign(b);
+}
