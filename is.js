d236d30f37829feea4a823b8b75dba3bcd21d155
ajaberi <adnan.ajaberi@gmail.com>
2023-11-27 14:55:02 +0300

    done

--- is.js
+++ is.js
@@ -1,50 +1,47 @@
-const is = {
-  num: function (value) {
-    return typeof value === "number";
-  },
-  nan: function (value) {
-    return Number.isNaN(value);
-  },
-  str: function (value) {
-    return typeof value === "string";
-  },
-  bool: function (value) {
-    return typeof value === "boolean";
-  },
-  undef: function (value) {
-    return typeof value === "undefined";
-  },
-  def: function (value) {
-    return typeof value !== "undefined";
-  },
-  arr: function (value) {
-    return Array.isArray(value);
-  },
-  obj: function (value) {
-    return typeof value === "object" && value !== null && !Array.isArray(value);
-  },
-  fun: function (value) {
-    return typeof value === "function";
-  },
-  truthy: function (value) {
-    return !!value;
-  },
-  falsy: function (value) {
-    return !value;
-  },
+is.num = function (value) {
+  return typeof value === "number";
 };
+is.nan = function (value) {
+  return Number.isNaN(value);
+};
+is.str = function (value) {
+  return typeof value === "string";
+};
+is.bool = function (value) {
+  return typeof value === "boolean";
+};
+is.undef = function (value) {
+  return typeof value === "undefined";
+};
+is.def = function (value) {
+  return typeof value !== "undefined";
+};
+is.arr = function (value) {
+  return Array.isArray(value);
+};
+is.obj = function (value) {
+  return typeof value === "object" && value !== null && !Array.isArray(value);
+};
+is.fun = function (value) {
+  return typeof value === "function";
+};
+is.truthy = function (value) {
+  return !!value;
+};
+is.falsy = function (value) {
+  return !value;
+};
+
 /*
-const is = {
-    num: value => typeof value === 'number',
-    nan: value => Number.isNaN(value),
-    str: value => typeof value === 'string',
-    bool: value => typeof value === 'boolean',
-    undef: value => typeof value === 'undefined',
-    def: value => typeof value !== 'undefined',
-    arr: value => Array.isArray(value),
-    obj: value => typeof value === 'object' && value !== null && !Array.isArray(value),
-    fun: value => typeof value === 'function',
-    truthy: value => !!value,
-    falsy: value => !value
-};
+    is.num: value => typeof value === 'number',
+    is.nan: value => Number.isNaN(value),
+    is.str: value => typeof value === 'string',
+    is.bool: value => typeof value === 'boolean',
+    is.undef: value => typeof value === 'undefined',
+    is.def: value => typeof value !== 'undefined',
+    is.arr: value => Array.isArray(value),
+    is.obj: value => typeof value === 'object' && value !== null && !Array.isArray(value),
+    is.fun: value => typeof value === 'function',
+    is.truthy: value => !!value,
+    is.falsy: value => !value
 */
