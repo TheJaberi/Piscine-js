0be76cc18b31aa911967903059fb2e5301ea55e8
ajaberi <adnan.ajaberi@gmail.com>
2023-11-27 12:56:51 +0300

    done

---
+++ dog-years.js
@@ -0,0 +1,16 @@
+const orbitalPeriods = {
+  earth: 1.0,
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132,
+};
+
+function dogYears(planet, ageInSeconds) {
+  const earthYears = ageInSeconds / 31557600; // Convert seconds to Earth years
+  const planetYears = earthYears / orbitalPeriods[planet]; // Convert Earth years to planet years
+  return planetYears * 7; // Convert to dog years
+}
