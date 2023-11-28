99620a6d4bc957cb439841aeb9324c8f38607b50
ajaberi <adnan.ajaberi@gmail.com>
2023-11-28 14:23:49 +0300

    done

--- repeat.js
+++ repeat.js
@@ -1,9 +1,7 @@
 function repeat(str, num) {
-  if (num <= 0) {
-    return "";
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += str;
   }
-  for (let i = 0; i < num - 1; i++) {
-    str += str;
-  }
-  return str;
+  return result;
 }
