9712a8a663e0967bc5d84f34148cbd6894d7e6c2
ajaberi <adnan.ajaberi@gmail.com>
2023-11-29 13:53:57 +0300

    done

--- cut-corners.js
+++ cut-corners.js
@@ -1,27 +1,44 @@
 function round(number) {
-  let numStr = number.toString();
-  let [intPart, decimalPart] = numStr.split(".");
-  decimalPart = decimalPart ? `0.${decimalPart}` : "0";
-  return decimalPart >= 0.5 ? Number(intPart) + 1 : Number(intPart);
+  const { intPart, decimalPart } = separateNumber(number);
+  if (number >= 0) {
+    return decimalPart >= 0.5 ? intPart + 1 : intPart;
+  } else {
+    return -decimalPart >= 0.5 ? intPart - 1 : intPart;
+  }
 }
 
 function ceil(number) {
-  let numStr = number.toString();
-  if (numStr.includes(".")) {
-    let [intPart] = numStr.split(".");
-    return Number(intPart) + 1;
+  const { intPart, decimalPart } = separateNumber(number);
+  if (number >= 0) {
+    return decimalPart > 0 ? intPart + 1 : intPart;
+  } else {
+    return intPart;
   }
-  return number;
 }
 
 function floor(number) {
-  let numStr = number.toString();
-  let [intPart] = numStr.split(".");
-  return Number(intPart);
+  const { intPart, decimalPart } = separateNumber(number);
+  if (number >= 0) {
+    return intPart;
+  } else {
+    return decimalPart < 0 ? intPart - 1 : intPart;
+  }
 }
 
 function trunc(number) {
-  let numStr = number.toString();
-  let [intPart] = numStr.split(".");
-  return Number(intPart);
+  const { intPart } = separateNumber(number);
+  return intPart;
+}
+
+function separateNumber(num) {
+  let negative = num < 0;
+  num = Math.abs(num);
+  let intPart = 0;
+  while (intPart < num) intPart++;
+
+  let decimalPart = num - intPart;
+  return {
+    intPart: negative ? -intPart : intPart,
+    decimalPart: negative ? -decimalPart : decimalPart,
+  };
 }
