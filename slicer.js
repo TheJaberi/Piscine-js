90165fe41912380731c9cac346d60bc513cbb5f3
ajaberi <adnan.ajaberi@gmail.com>
2023-11-29 12:19:13 +0300

    done

--- slicer.js
+++ slicer.js
@@ -2,14 +2,13 @@ function slice(arrOrStr, start, end) {
   let result = Array.isArray(arrOrStr) ? [] : "";
   let length = arrOrStr.length;
 
-  // Handling negative indices
-  start = start < 0 ? Math.max(length + start, 0) : Math.min(start, length);
-  end =
-    end === undefined
-      ? length
-      : end < 0
-      ? Math.max(length + end, 0)
-      : Math.min(end, length);
+  // Adjusting negative indices
+  start = start < 0 ? length + start : start;
+  end = end === undefined ? length : end < 0 ? length + end : end;
+
+  // Clamping start and end to the valid range
+  start = start > length ? length : start;
+  end = end > length ? length : end < 0 ? 0 : end;
 
   for (let i = start; i < end; i++) {
     if (Array.isArray(arrOrStr)) {
