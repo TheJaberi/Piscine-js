0adf1003f67a541a23c8e6ceb9fd30ce3e6d06a2
ajaberi <adnan.ajaberi@gmail.com>
2023-12-04 11:45:17 +0300

    The day

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -2,6 +2,15 @@ function dayOfTheYear(date) {
   // Ensure the input is a Date object
   let inputDate = new Date(date);
 
+  // Handling the year 1 scenario
+  if (
+    inputDate.getFullYear() === 1 &&
+    inputDate.getMonth() === 0 &&
+    inputDate.getDate() === 1
+  ) {
+    return 1;
+  }
+
   // Create a new date for the first day of the year
   let firstDayOfYear = new Date(inputDate.getFullYear(), 0, 1);
 
